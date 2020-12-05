785f4858e99c50dc48cfd5e0e529041c2af01fbf
Eden Gilligan <edengilligan@Edens-MBP.modem>
2020-12-05 16:52:26 +1100

    submit and val working

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 console.log("app is running"); 
+const todoList = [{title: 'to do', done: false}]; 
+
+const onTodoFormSubmit = (event) => {
+event.preventDefault();
+console.log("form submit"); 
+const $todo = $('input[name="todo"]');
+console.log($todo.val()); 
+const todoItem = { title: $todo.val(), done: false}; 
+todoList.push(todoItem);
+console.log("todoList", todoList);
+}; 
+
+$("#todo-form").on ("submit", onTodoFormSubmit); 
